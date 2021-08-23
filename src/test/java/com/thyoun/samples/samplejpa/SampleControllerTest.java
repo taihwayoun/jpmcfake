65245fc8560ed45c152c11565bb5a19b5da16d64
taihwayoun <taihwayoun@gmail.com>
2021-08-23 18:40:09 -0500

    Test files were updated.

--- src/test/java/com/thyoun/samples/samplejpa/SampleControllerTest.java
+++ src/test/java/com/thyoun/samples/samplejpa/SampleControllerTest.java
@@ -1,28 +1,39 @@
 package com.thyoun.samples.samplejpa;
 
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import io.restassured.RestAssured;
-import static org.hamcrest.CoreMatchers.equalTo;;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static io.restassured.module.mockmvc.matcher.RestAssuredMockMvcMatchers.*;
 
 public class SampleControllerTest {
 
+	@LocalServerPort
+	private static int port=9090;
+	
 	@BeforeAll
 	public static void init() {
 		RestAssured.baseURI="http://localhost";
-		RestAssured.port=9090;
+		RestAssured.port=port;
 	}
 
 	@Test
 	public void testUserFetchesSuccess() {
-		RestAssured.get("/employee")
+		given()
+			.standaloneSetup(new SampleController())
+		.when()
+			.get("/employee")
 		.then()
-		.body("id",  equalTo(3))
-		.body("fn", equalTo("James"))
-		.body("ln", equalTo("Jones"))
-		.body("email", equalTo("james.jones@chase.com"));
+			.body("id",  equalTo(3))
+			.body("fn", equalTo("James"))
+			.body("ln", equalTo("Jones"))
+			.body("email", equalTo("james.jones@chase.com"));
 		
 	}
 }
